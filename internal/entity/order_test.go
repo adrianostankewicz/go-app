0abaa2d1e3ce522e3065b9e4c274e50cebd188a8
Adriano Stankewicz <adriano.stankewicz@gmail.com>
2023-11-22 06:57:59 -0300

    add order tests

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -1,10 +1,29 @@
 package entity
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestIfItGetsAnErrorIfIDIsBlank(t *testing.T) {
 	order := Order{}
-	if order.Validate() == nil {
-		t.Error("Id is required")
-	}
+	assert.Error(t, order.Validate(), "id is required")
+}
+func TestIfItGetsAnErrorIfPriceIsBlank(t *testing.T) {
+	order := Order{ID: "123"}
+	assert.Error(t, order.Validate(), "price must be greater than zero")
+}
+func TestIfItGetsAnErrorIfTaxIsBlank(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0}
+	assert.Error(t, order.Validate(), "invalid tax")
+}
+func Test(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0, Tax: 1.0}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	order.CalculateFinalPrice()
+	assert.Equal(t, 11.0, order.FinalPrice)
 }
